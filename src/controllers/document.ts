c7dd66d5f57ba480061ff91eaac65e379f9c9783
alexlop29 <alexander.lopez@owasp.org>
2024-01-21 14:04:30 -0500

    Run prettier

--- src/controllers/document.ts
+++ src/controllers/document.ts
@@ -1,9 +1,21 @@
 class Document {
-  constructor(
-    private readonly name: string,
-    private readonly user: string,
-    private readonly contents: string, // replace with multer // will contain file contents
-  ) {}
-}
+    constructor(
+        private readonly name: string,
+        private readonly user: string,
+        private readonly contents: string, // replace with multer // will contain file contents
+    ) {}
+
+    get(){
+
+    };
+
+    validate() {
+        // validates the document's information
+    };
+
+    save() {
+        // saves the file to the database
+    };
+};
 
 export { Document };
